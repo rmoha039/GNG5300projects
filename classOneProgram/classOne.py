d3965c4ef8de2a535bcb57a0775f1fea79ab531b
Jiafeng-Li-Git <jli740@uottawa.ca>
2022-09-13 19:54:55 -0400

    submit class one program

--- classOneProgram/classOne.py
+++ classOneProgram/classOne.py
@@ -1,19 +1,23 @@
 num1 = float(input("Input the first number: "))
 num2 = float(input("Input the second number: "))
+operation = input("Input the operation(+, -, *, **, /): ")
 
-addition_result = num1 + num2
-minus_result = num1 - num2
-multi_result = num1 * num2
-power_result = num1 ** num2
+operation_result = None
+if operation == "+":
+    operation_result = num1 + num2
+elif operation == "-":
+    operation_result = num1 - num2
+elif operation == "*":
+    operation_result = num1 * num2
+elif operation == "**":
+    operation_result = num1 ** num2
+elif operation == "/":
+    try:
+        divi_result = num1 / num2
+    except ZeroDivisionError as e:
+        print("num2 can't be zero when num1 divide num2")
+        operation_result = "infinity"
+else:
+    print("no such operation")
 
-try:
-    divi_result = num1/num2
-except ZeroDivisionError as e:
-    print("num2 can't be zero when num1 divide num2")
-    divi_result = "infinity"
-
-print("num1 + num2 =", addition_result)
-print("num1 - num2 =", minus_result)
-print("num1 * num2 =", multi_result)
-print("num1 ** num2 =", power_result)
-print("num1 / num2 =", divi_result)
+print("The equation result is: ", operation_result)
